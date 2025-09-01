63c19432feb9ec73c0315a4cf6926e4cae9784b3
Spencer Peters <st10357026@vcconnect.edu.za>
2025-09-01 19:36:29 +0200

    Added Node and List

---
+++ StoryLinkedList.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game_Script_Manager
+{
+    public class StoryLinkedList
+    {
+        public Node? Head { get; private set; }
+
+        public void Add(int number, string text)
+        {
+            Node newNode = new Node(number, text);
+            if(Head == null)
+            {
+                Head = newNode;
+            }
+            else
+            {
+                Node current = Head;
+                while (current.Next != null)
+                {
+                    current = current.Next;
+                }
+
+                current.Next = newNode;
+            }
+        }
+
+        public void Sort()
+        {
+            if (Head == null) return;
+
+            bool swapped;
+            do
+            {
+                swapped = false;
+                Node? current = Head;
+                while (current?.Next != null)
+                {
+                    if (current.StoryNumber > current.Next.StoryNumber)
+                    {
+
+                        (current.StoryNumber, current.Next.StoryNumber) = (current.Next.StoryNumber, current.StoryNumber);
+                        (current.StoryText, current.Next.StoryText) = (current.Next.StoryText, current.StoryText);
+                        swapped = true;
+                    }
+                    current = current.Next;
+                }
+            } while (swapped);
+        }
+
+        public List<string> ToList()
+        {
+            List<string> list = new();
+            Node? current = Head;
+            while(current != null)
+            {
+                list.Add($"{current.StoryNumber}: {current.StoryText}");
+                current = current.Next;
+            }
+            return list;
+        }
+    }
+}
--- obj/Debug/net8.0-windows/Game Script Manager.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/Game Script Manager.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Game Script Manager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a52034d3c014ce4ed4aa0b0f7b477d7b8fd52cc")]
 [assembly: System.Reflection.AssemblyProductAttribute("Game Script Manager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Game Script Manager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
