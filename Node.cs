2c313538598d43f2659441884a722136772f3e59
Spencer Peters <st10357026@vcconnect.edu.za>
2025-09-01 19:24:54 +0200

    Initial Commit

---
+++ Node.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game_Script_Manager
+{
+    public class Node
+    {
+        public int StoryNumber { get; set; }
+        public string StoryText { get; set; }
+        public Node? Next { get; set; }
+
+        public Node (int number, string text)
+        {
+            StoryNumber = number;
+            StoryText = text;
+            Next = null;
+        }
+    }
+}
